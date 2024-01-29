ede8109a9aae521c7c5b493ba7f8d1c1e75ab039
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-01-29 17:21:14 +0900

    fix: url path variable 처리 수정

--- member/urls.py
+++ member/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 from .views import get_members, get_member_for_id
 
 urlpatterns = [
-  url('', get_members),
-  url('<int:id>/', get_member_for_id)
+  url(r'^$', get_members),
+  url(r'(?P<id>\d+)/$', get_member_for_id),
 ]
