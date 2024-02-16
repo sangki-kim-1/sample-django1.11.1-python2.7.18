ddb89748f6e376a70eeee471b4b1ffb456384b3b
김동영 <43669379+idean3885@users.noreply.github.com>
2024-02-16 12:21:56 +0900

    Update urls.py

--- restapi/urls.py
+++ restapi/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^members/', include('member.urls'))
+    url(r'^members/v1', include('member.urls'))
 ]
