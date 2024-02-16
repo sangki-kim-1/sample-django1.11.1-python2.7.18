c1b44a8018b08271c488d4d009c660a7a850fe26
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-02-16 15:36:41 +0900

    fix: CORS 전체 허용으로 변경

--- restapi/settings.py
+++ restapi/settings.py
@@ -142,5 +142,6 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_WHITELIST = ['http://10.78.95.55:3000','http://localhost:3000']
+# CORS_ORIGIN_WHITELIST = ['http://10.78.95.55:3000','http://localhost:3000']
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
