57a3398cdfefb4d40d9e3f1a0f3e34419d4964f7
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-02-19 15:36:24 +0900

    fix: 샘플 도커이미지 이름 변경

--- runScript.sh
+++ runScript.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-BUILD_NAME=sample-python2-django-api-db
+BUILD_NAME=sample-django1.11.1-python2.7.18
 
 docker rmi $BUILD_NAME
 docker build -t $BUILD_NAME .
 
 # foreground -> script exit = docker process stop
-docker run --rm -p 8000:8000 --name $BUILD_NAME -v ./:/app $BUILD_NAME
+docker run --rm -p 8000:8000 --name $BUILD_NAME $BUILD_NAME
+
+# for docker data remaining local
+#docker run --rm -p 8000:8000 --name $BUILD_NAME -v ./:/app $BUILD_NAME
 
 # background -> must docker stop manually for process exit
 # docker run -d --rm -p 8000:8000 --name $BUILD_NAME $BUILD_NAME
